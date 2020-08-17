80e7eaeef73a880600c321435fe60988a72a8dc9
Unknown <unknown@example.com>
2020-08-18 00:24:18 +0200

    challenge application

---
+++ Wyzwanie/Program.cs
@@ -0,0 +1,34 @@
+﻿using NETCore.Encrypt;
+using System;
+using System.Configuration;
+using Console = Colorful.Console;
+
+namespace Wyzwanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteAscii("kursdotnet.pl");
+            Method();
+        }
+
+        private static void Method()
+        {
+            Console.WriteLine("Cześć. Witaj w wyzwaniu Kurs .NET. Aby wziąć udział w grze podaj swój adres email.");
+
+            Console.Write("email:");
+            var email = Console.ReadLine();
+
+            var key = ConfigurationManager.AppSettings["key"];
+            var encrypted = EncryptProvider.AESEncrypt(email, key);
+
+            var group = EncryptProvider.AESDecrypt(ConfigurationManager.AppSettings["group"], key);
+
+            Console.WriteLine($"Dołącz do grupy:");
+            Console.WriteLine(group);
+            Console.WriteLine($"Twoje hasło: { encrypted}");
+            Console.ReadLine();
+        }
+    }
+}
